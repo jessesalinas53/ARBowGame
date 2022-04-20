60d478be4c6e49b376ff55b300257d9b5d5e3845
Henry Roberts <whroberts5@gmail.com>
2022-04-20 17:52:55 -0500

    camera lock to camera

--- Assets/ExampleAssets/Scripts/Webcam.cs
+++ Assets/ExampleAssets/Scripts/Webcam.cs
@@ -10,14 +10,26 @@ public class Webcam : MonoBehaviour
     private RawImage _rawImage;
     private WebCamTexture _webCamTexture;
 
+    private Camera _camera = null;
+    private RectTransform _rectTransform = null;
+
+    private void Awake()
+    {
+        _camera = FindObjectOfType<Camera>();
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
     void Start()
     {
         StartWebCam();
+
+        Debug.Log(_rectTransform.sizeDelta);
+        Debug.Log(_camera.aspect);
     }
 
     public void StartWebCam()
     {
-        _rawImage = GetComponent<RawImage>();
+        _rawImage = GetComponentInChildren<RawImage>();
         _webCamTexture = new WebCamTexture();
         _rawImage.material.mainTexture = _webCamTexture;
         _webCamTexture.Play();
