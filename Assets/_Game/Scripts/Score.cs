87c71dadd763ab3a3c9b47ec9bccc07fb8651ec7
Henry Roberts <whroberts5@gmail.com>
2022-04-21 04:46:09 -0500

    full project reorganization

---
+++ Assets/_Game/Scripts/Score.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public int score = 0;
+    public int highScore = 0;
+    public TMP_Text scoreText;
+    public TMP_Text highScoreText;
+    public TMP_Text postGameScoreText;
+    public TMP_Text menuHighScoreText;
+
+    public GameObject startMenuPnl;
+    public GameObject restartPnl;
+
+    Timer timerScript;
+
+    private void Start()
+    {
+        score = 0;
+        scoreText.text = "Score: 0";
+        highScore = PlayerPrefs.GetInt("High Score");
+        highScoreText.text = "High Score: " + highScore;
+        menuHighScoreText.text = "High Score: " + highScore;
+
+        timerScript = GetComponent<Timer>();
+    }
+    private void Update()
+    {
+        if(timerScript.timeRemaining <= 0)
+        {
+            UpdateScoreText();
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score: " + score;
+        //Debug.Log("Score: " + score);
+    }
+
+    public void UpdateScoreText()
+    {
+        postGameScoreText.text = "Your Score: " + score;
+
+        if(score > PlayerPrefs.GetInt("High Score", 0))
+        {
+            Debug.Log("High Score set.");
+            PlayerPrefs.SetInt("High Score", highScore);
+            highScore = score;
+            highScoreText.text = "High Score: " + highScore;
+        }
+    }
+
+    public void StartButton()
+    {
+        startMenuPnl.SetActive(false);
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        scoreText.text = "Score: 0";
+    }
+
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey("High Score");
+        highScore = 0;
+        score = 0;
+        menuHighScoreText.text = "High Score: " + highScore;
+        highScoreText.text = "High Score: " + highScore;
+    }
+
+    public void ReturnToMenu()
+    {
+        startMenuPnl.SetActive(true);
+        menuHighScoreText.text = "High Score: " + highScore;
+        restartPnl.SetActive(false);
+    }
+}
---
+++ Assets/_Game/Scripts/Timer.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 30;
+    public bool timerIsRunning = false;
+    public TMP_Text timerText;
+    public GameObject restartPanel;
+    public GameObject gameCanvas;
+
+    private float initTime;
+
+    private void Start()
+    {
+        initTime = timeRemaining;
+    }
+
+    void Update()
+    {
+        if(timerIsRunning == true)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                restartPanel.SetActive(true);
+                gameCanvas.SetActive(false);
+                timeRemaining = initTime;
+            }
+        }
+        DisplayText(timeRemaining);
+    }
+
+    void DisplayText(float timeToDisplay)
+    {
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        float milliSeconds = (timeToDisplay % 1) * 100;
+        timerText.text = string.Format("Time: {0:00}:{1:00}", seconds, milliSeconds);
+    }
+
+    public void StartTimer()
+    {
+        // auto setting timeRemaining regardless of user input
+        //timeRemaining = 5;
+        timerIsRunning = true;
+        restartPanel.SetActive(false);
+    }
+}
